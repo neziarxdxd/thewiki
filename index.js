3d85a25ac96302fbf741ca3f5e9252d86d7401f6
raizengxd <rickdavidsangalang@gmail.com>
2020-09-18 01:32:13 +0800

    done

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express")
+const fetch = require('node-fetch');
 const app =express();
 let port = process.env.PORT || 3000;
+
+
+app.get("/wike/:search",(req,res) => {
+    let url = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${req.params.search}`;
+
+
+    let settings = { method: "Get" };
+
+    y= fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            var getData = json["query"]["pages"]
+            var keys = Object.keys(getData);
+            res.send({"messages":getData[keys]["extract"]})
+    });
+});
 
 
 app.get("/",(req,res) => {
@@ -9,3 +26,11 @@ app.get("/",(req,res) => {
 app.listen(port,()=>{
     console.log("EXAMPLE LANG TOL");
 });
+
+
+
+
+
+
+
+
